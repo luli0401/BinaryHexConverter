cca2eff0fd94be0dfd6f46623ec838c313fa3465
Lu Li <luli0401@gmail.com>
2018-10-12 16:10:52 -0600

    Add project files.

---
+++ BinaryHexConverter/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using BinaryHexConverter.Processor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BinaryHexConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            HexProcessor = new HexProcessor();
+            BinaryProcessor = new BinaryProcessor();
+            IntegerProcessor = new IntegerProcessor();
+        }
+
+        public BinaryProcessor BinaryProcessor { get; set; }
+        public HexProcessor HexProcessor { get; set; }
+        public IntegerProcessor IntegerProcessor { get; set; }
+
+        private void ConvertExecute(object sender, RoutedEventArgs e)
+        {
+            var input = InputTextBox.Text;
+
+            if (IsHex.IsChecked == true)
+            {
+                BinaryTextBox.Text = HexProcessor.ConvertToBinary(input);
+                IntegerTextBox.Text = HexProcessor.ConvertToInteger(input);
+            }
+            else if (IsBinary.IsChecked == true)
+            {
+                IntegerTextBox.Text = string.Empty;
+                HexTextBox.Text = string.Empty;
+            }
+            else if (IsInteger.IsChecked == true)
+            {
+
+            }
+        }
+
+        private void RefreshExecute(object sender, RoutedEventArgs e)
+        {
+            BinaryTextBox.Text = string.Empty;
+            IntegerTextBox.Text = string.Empty;
+            HexTextBox.Text = string.Empty;
+            InputTextBox.Text = string.Empty;
+        }
+    }
+}
---
+++ BinaryHexConverter/Processor/HexProcessor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryHexConverter.Processor
+{
+    public class HexProcessor
+    {
+        internal string ConvertToBinary(string input)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                result = Convert.ToString(Convert.ToInt32(input, 16), 2);
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            return result;
+        }
+
+        internal string ConvertToInteger(string input)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                result = Convert.ToInt32(input, 16).ToString();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return result;
+        }
+    }
+}
